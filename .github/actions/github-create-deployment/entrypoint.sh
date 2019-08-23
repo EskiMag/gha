20a9f2b0bd8fd421bbb29a69a09d0d8d8182034c
Štefan Ľupták <stefan.luptak@smarkup.com>
2019-08-23 14:42:58 +0200

    Do not require context to be success

--- .github/actions/github-create-deployment/entrypoint.sh
+++ .github/actions/github-create-deployment/entrypoint.sh
@@ -6,6 +6,7 @@ echo "hub api -X POST repos/$GITHUB_REPOSITORY/deployments -F ref=$GITHUB_REF de
 
 hub api -X POST "repos/$GITHUB_REPOSITORY/deployments" \
   -F ref=$GITHUB_REF \
+  required_contexts="[]" \
   description=$DEPLOYMENT_DESCRIPTION
 
 # hub api -X POST https://api.github.com/repos/smarkup/web/deployments/164190170/statuses \
